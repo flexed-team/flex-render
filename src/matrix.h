d0b20ed5c3bfdde9f4fb01e109619299aef9e696
nt4f04uNd <danbosszero@gmail.com>
2019-11-07 17:20:48 +0300

    Fixes

--- src/matrix.h
+++ src/matrix.h
@@ -15,57 +15,69 @@ class Matrix
 public:
 	union {
 		/// height and width
-		struct { int w, h; };
-		/// n is count of cols, m is count of rows, 
-		struct { int n, m; };
+		struct
+		{
+			unsigned int w, h;
+		};
+		/// n is count of cols, m is count of rows,
+		struct
+		{
+			unsigned int n, m;
+		};
 	};
 
 	/// Matrix values array
 	std::vector<t> v = std::vector<t>();
 
 	// Fills matrix with zeros
-	Matrix(int _w, int _h) : w(_w), h(_h), v(_w * _h, 0) {}
-	Matrix(int _w, int _h, t* _v) : w(_w), h(_h), v(_v, _v + _w * _h) {}
+	Matrix(unsigned int _w, unsigned int _h) : w(_w), h(_h), v(_w* _h, 0) {}
+	Matrix(unsigned int _w, unsigned int _h, t* _v) : w(_w), h(_h), v(_v, _v + _w * _h) {}
+	Matrix(unsigned int _w, unsigned  int _h, std::vector<t> _v) : w(_w), h(_h), v(_v.begin(), _v.end()) {}
+	Matrix(unsigned int _w, unsigned  int _h, std::vector<t>* _v) : w(_w), h(_h), v(_v->begin(), _v->end()) {}
 
 	// Methods ------------------------------------
 
 	/// Gets matrix values array size
 	inline int length() const { return w * h; }
 
-
 	/// Inserts row to matrix
-	void insert_row(t* rowv, int roww) {
+	void insert_row(t* rowv, int roww)
+	{
 		assert(w == roww);
-		const int add = +w * h;
-		for (int i = 0; i < w; i++) {
+		const unsigned int add = +w * h;
+		for (int i = 0; i < w; i++)
+		{
 			v.insert(v.begin() + add + i, rowv[i]);
 		}
 		h++;
 	}
 
 	/// Inserts col to matrix
-	void insert_col(t* colv, int colh) {
+	void insert_col(t* colv, int colh)
+	{
 		assert(h == colh);
 		w++;
-		const int add = w - 1;
-		for (int i = 0; i < w; i++) {
+		const unsigned int add = w - 1;
+		for (int i = 0; i < w; i++)
+		{
 			v.insert(v.begin() + add + i * w, colv[i]);
 		}
 	}
 
 	void log()
 	{
-		for (int i = 0; i < w * h; i++)
+		for (unsigned int i = 0; i < w * h; i++)
 		{
 			printf("%5d,", v[i]);
-			if (i % w == w - 1) std::cout << "\n";
+			if (i % w == w - 1)
+				std::cout << "\n";
 		}
 		std::cout << "\n";
 	}
 
-
 	// Unary minus
-	Matrix<t> operator -() {
+	Matrix<t> operator-()
+	{
 		t* _v = v.data();
 		for (int i = 0; i < w * h; i++)
 			_v[i] = -_v[i];
@@ -73,52 +85,59 @@ public:
 	}
 
 	// Int ------------------------------------
-	Matrix<t> operator +(int other) {
+	Matrix<t> operator+(int other)
+	{
 		t* _v = v.data();
 		for (int i = 0; i < w * h; i++)
 			_v[i] += other;
 		return Matrix<t>(w, h, _v);
 	}
-	Matrix<t> operator -(int other) {
+	Matrix<t> operator-(int other)
+	{
 		t* _v = v.data();
 		for (int i = 0; i < w * h; i++)
 			_v[i] -= other;
 		return Matrix<t>(w, h, _v);
 	}
-	Matrix<t> operator *(int other) {
+	Matrix<t> operator*(int other)
+	{
 		t* _v = v.data();
 		for (int i = 0; i < w * h; i++)
 			_v[i] *= other;
 		return Matrix<t>(w, h, _v);
 	}
-	Matrix<t> operator /(int other) {
+	Matrix<t> operator/(int other)
+	{
 		t* _v = v.data();
 		for (int i = 0; i < w * h; i++)
 			_v[i] /= other;
 		return Matrix<t>(w, h, _v);
 	}
 
-
 	// Float ------------------------------------
-	Matrix<t> operator +(float other) {
+	Matrix<t> operator+(float other)
+	{
 		t* _v = v.data();
 		for (int i = 0; i < w * h; i++)
 			_v[i] += other;
 		return Matrix<t>(w, h, _v);
 	}
-	Matrix<t> operator -(float other) {
+	Matrix<t> operator-(float other)
+	{
 		t* _v = v.data();
 		for (int i = 0; i < w * h; i++)
 			_v[i] -= other;
 		return Matrix<t>(w, h, _v);
 	}
-	Matrix<t> operator *(float other) {
+	Matrix<t> operator*(float other)
+	{
 		t* _v = v.data();
 		for (int i = 0; i < w * h; i++)
 			_v[i] *= other;
 		return Matrix<t>(w, h, _v);
 	}
-	Matrix<t> operator /(float other) {
+	Matrix<t> operator/(float other)
+	{
 		t* _v = v.data();
 		for (int i = 0; i < w * h; i++)
 			_v[i] /= other;
@@ -126,40 +145,44 @@ public:
 	}
 
 	// Matrix operations ------------------------------------
-	Matrix<t> operator +(Matrix<t>& other) {
+	Matrix<t> operator+(Matrix<t>& other)
+	{
 		t* _v = v.data();
 		for (int i = 0; i < w * h; i++)
 			_v[i] += other.v[i];
 		return Matrix<t>(w, h, _v);
 	}
-	Matrix<t> operator -(Matrix<t>& other) {
+	Matrix<t> operator-(Matrix<t>& other)
+	{
 		check_sizes(other);
 		t* _v = v.data();
 		for (int i = 0; i < w * h; i++)
 			_v[i] -= other.v[i];
 		return Matrix<t>(w, h, _v);
 	}
-	Matrix<t> operator *(Matrix<t>& other) {
+	Matrix<t> operator*(Matrix<t>& other)
+	{
 		assert(w == other.h);
-      t *_v = new t[h * other.w];
-      for (int i = 0; i < h; i++)
-         for (int j = 0; j < other.w; j++)
-            for (int k = 0; k < w; k++)
-               _v[j + i * other.w] += v[i * w + k] * other.v[k * other.w + j];
+		std::vector<t> _v = std::vector<t>(h * other.w);
+		for (unsigned int i = 0; i < h; i++)
+			for (unsigned int j = 0; j < other.w; j++)
+				for (unsigned int k = 0; k < w; k++)
+					_v[j + i * other.w] += v[i * w + k] * other.v[k * other.w + j];
 
-		return Matrix<t>(other.w, h, _v);
+		return Matrix<t>(other.w, h, &_v);
 	}
 
 private:
 	/// Checks other matrix for size identity
 	/// Uses assert
-	inline void check_sizes(Matrix<t>& m) { assert(w == m.w); assert(h == m.h); }
-
+	inline void check_sizes(Matrix<t>& m)
+	{
+		assert(w == m.w);
+		assert(h == m.h);
+	}
 };
 
-
-typedef  Matrix<int> Mati;
-typedef  Matrix<float> Matf;
-
+typedef Matrix<int> Mati;
+typedef Matrix<float> Matf;
 
 #endif //__MATRIX_H__
